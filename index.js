8905f5d7c9f59b408023b53d73601c16a50af9d9
Linganator <gregory.s.ling@gmail.com>
2026-06-10 18:21:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post("/webhook", async (req, res) => {
 async function sendMessage(senderId, text) {
   const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-  const url = https://graph.facebook.com/v25.0/me/messages?access_token=${PAGE_ACCESS_TOKEN};
+  const url = `https://graph.facebook.com/v25.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`;
 
   const response = await fetch(url, {
     method: "POST",
