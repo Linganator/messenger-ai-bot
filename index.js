682b5795e498de45e0f9c912303f401b8d471aac
Linganator <gregory.s.ling@gmail.com>
2026-06-10 12:28:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
-
-const VERIFY_TOKEN = "bennington_verify_token";
-
-// Meta webhook verification
-app.get("/webhook", (req, res) => {
-  const mode = req.query["hub.mode"];
-  const token = req.query["hub.verify_token"];
-  const challenge = req.query["hub.challenge"];
-
-  if (mode === "subscribe" && token === VERIFY_TOKEN) {
-    return res.status(200).send(challenge);
-  }
-
-  res.sendStatus(403);
+app.get("/", (req, res) => {
+  res.send("HOME WORKS");
 });
 
-// Receive messages
-app.post("/webhook", (req, res) => {
-  console.log("Incoming:", JSON.stringify(req.body));
-  res.sendStatus(200);
+app.get("/webhook", (req, res) => {
+  res.send("WEBHOOK WORKS");
 });
 
 const PORT = process.env.PORT || 3000;
